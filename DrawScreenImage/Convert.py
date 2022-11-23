89f1feb94f74a7fad3dce8e2e8b5246cbb7bd407
juniorjar35 <aymandb2006@gmail.com>
2022-11-23 12:20:50 +0200

    Extend converter script

--- DrawScreenImage/Convert.py
+++ DrawScreenImage/Convert.py
@@ -1,6 +1,11 @@
 from PIL import Image
+import sys
 
-image = Image.open("scaled_indexed.bmp")
+if len(sys.argv) < 2:
+    print("Not enough arguments")
+    sys.exit(1)
+
+image = Image.open(sys.argv[1])
 pixels = image.load()
 
 w = image.width
@@ -14,6 +19,6 @@ for y in range(h):
     for x in range(w):
         bytesWritten += out_file.write(int.to_bytes(pixels[x,y], 1, 'little'))
 
-
+out_file.flush()
 out_file.close()
 print(f"{bytesWritten} bytes written!")
